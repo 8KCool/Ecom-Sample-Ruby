83abcd0ef9194fd43378b23a5247e25896e66d73
8klancer <8klancer@gmail.com>
2018-04-30 17:05:39 -0300

    User can create store

--- spec/features/customer_admin_store_spec.rb
+++ spec/features/customer_admin_store_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 feature 'Customer admin stores' do
-  scenario 'creating a new store' do
+  scenario 'Visiting new store page' do
     visit new_store_path
 
     expect(page).to have_content("Crie sua loja!")
+  end
+
+  scenario 'creating a new store' do
+    visit new_store_path
+    
+    fill_in 'Name', with: 'Besni'
+    fill_in 'Slogan', with: 'Dedicação total a você'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Loja criada com sucesso!')
+    expect(page).to have_content('Besni')
+    expect(page).to have_content('Dedicação total a você')
   end
 end
