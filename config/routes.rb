ba8e3dbdb62090f98b92e7bb746151d6248d0574
8klancer <8klancer@gmail.com>
2018-05-08 12:39:20 -0300

    Added bootstrap and create navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "products#index"
   get '/stores/:id/products/new', to: "products#new"
-  resources :stores, only: [:new, :create, :show]
+  resources :stores, only: [:index, :new, :create, :show]
   resources :products, only: [:show, :create]
 
 end
